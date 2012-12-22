ac18dd1d3ce4df6c79e964e0ac533a5a2a9a3dc0
deepakvenkat <deepakvenkat@live.com>
2012-12-22 16:10:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CashEnvelope::Application.config.secret_token = '5b3eb6cf0dbfbab78814fc8086094b4242e931f51b7aae8dc8fedf7efcd63f90230534fa869c373a583f80d5c6b950100c17f62f12bf3fca543f947337d362bd'
